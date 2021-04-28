282d93933cf5b5faa2a763b81d576f33884b4e87
Mathis Bourdin <mathisbrdn@gmail.com>
2021-04-28 10:17:20 +0200

    Boulette

--- pyheatmy/utils.py
+++ pyheatmy/utils.py
@@ -3,7 +3,7 @@ from numba import njit
 
 from .solver import solver, tri_product
 
-LAMBDA_W = .6071
+LAMBDA_W = 0.6071
 RHO_W = 1000
 C_W = 4185
 
@@ -14,61 +14,89 @@ PARAM_LIST = (
     "rhos_cs",
 )
 
+
 @njit()
-def compute_next_temp(moinslog10K, n, lambda_s, rhos_cs, dt, dz, temp_prev, H, H_prev, t0, tn, alpha = .7):
+def compute_next_temp(
+    moinslog10K, n, lambda_s, rhos_cs, dt, dz, temp_prev, H, H_prev, t0, tn, alpha=0.7
+):
     N = H_prev.size
     H = H.astype(float32)
     H_prev = H_prev.astype(float32)
     temp_prev = temp_prev.astype(float32)
 
-    rho_mc_m = n*RHO_W*C_W + (1-n)*rhos_cs
-    K = 10.**-moinslog10K
-    lambda_m = (n*(LAMBDA_W)**.5 + (1.-n)*(lambda_s)**.5)**2
+    rho_mc_m = n * RHO_W * C_W + (1 - n) * rhos_cs
+    K = 10.0 ** -moinslog10K
+    lambda_m = (n * (LAMBDA_W) ** 0.5 + (1.0 - n) * (lambda_s) ** 0.5) ** 2
 
-    ke = lambda_m/rho_mc_m
-    ae = RHO_W*C_W*K/rho_mc_m
-    dH = (H_prev[1:]-H_prev[:-1])/dz
+    ke = lambda_m / rho_mc_m
+    ae = RHO_W * C_W * K / rho_mc_m
+    dH = (H_prev[1:] - H_prev[:-1]) / dz
 
-    a = (-ke/dz**2 + dH*(.5*ae/dz)) * (1-alpha)
-    b = full(N, (1-alpha)*2*ke/dz**2 - 1/dt, dtype = float32)
-    c = (-ke/dz**2 - dH*(.5*ae/dz)) * (1-alpha)
+    a = (-ke / dz ** 2 + dH * (0.5 * ae / dz)) * (1 - alpha)
+    a[0] = -(1 - alpha) * (2 * ke / dz ** 2 - ae * dH[0] / (2 * dz))
+    a[-2] = -(1 - alpha) * (ke / dz ** 2 + ae * dH[-2] / (2 * dz))
+    b = full(N, (1 - alpha) * 2 * ke / dz ** 2 - 1 / dt, dtype=float32)
+    b[1] = -(1 - alpha) * (-2 * ke / dz ** 2) * (3 / 2) - 1 / dt
+    b[-2] = -(1 - alpha) * (-2 * ke / dz ** 2) * (3 / 2) - 1 / dt
+    c = (-ke / dz ** 2 - dH * (0.5 * ae / dz)) * (1 - alpha)
+    c[1] = -(1 - alpha) * (ke / dz ** 2 + ae * dH[1] / (2 * dz))
+    c[-1] = -(1 - alpha) * (2 * ke / dz ** 2 - ae * dH[-1] / (2 * dz))
 
     lim = tri_product(a, b, c, temp_prev)
     lim[0], lim[-1] = t0, tn
 
-    dH = (H[1:]-H[:-1])/dz
+    dH = (H[1:] - H[:-1]) / dz
 
-    a = (ke/dz**2-dH*(.5*ae/dz)) * alpha
-    a[-1] = 0.
-    b = full(N, -alpha*2*ke/dz**2 - 1/dt, dtype = float32)
-    b[0], b[-1] = 1., 1.
-    c = (ke/dz**2+dH*(.5*ae/dz)) * alpha
-    c[0] = 0.
+    a = (ke / dz ** 2 - dH * (0.5 * ae / dz)) * alpha
+    a[0] = alpha * (2 * ke / dz ** 2 - ae * dH[0] / (2 * dz))
+    a[-2] = alpha * (ke / dz ** 2 + ae * dH[-2] / (2 * dz))
+    a[-1] = 0.0
+    b = full(N, -alpha * 2 * ke / dz ** 2 - 1 / dt, dtype=float32)
+    b[0], b[-1] = 1.0, 1.0
+    b[1] = alpha * (-2 * ke / dz ** 2) * (3 / 2) - 1 / dt
+    b[-2] = alpha * (-2 * ke / dz ** 2) * (3 / 2) - 1 / dt
+    c = (ke / dz ** 2 + dH * (0.5 * ae / dz)) * alpha
+    c[0] = 0.0
+    c[1] = alpha * (ke / dz ** 2 + ae * dH[1] / (2 * dz))
+    c[-1] = alpha * (2 * ke / dz ** 2 - ae * dH[-1] / (2 * dz))
 
     return solver(a, b, c, lim)
 
+
 @njit
-def compute_next_h(K, Ss, dt, dz, H_prev, H0, Hn, alpha = .7):
+def compute_next_h(K, Ss, dt, dz, H_prev, H0, Hn, alpha=0.7):
     N = H_prev.size
     H_prev = H_prev.astype(float32)
 
-    k = K*(1-alpha)/dz**2
+    k = K * (1 - alpha) / dz ** 2
 
-    a = full(N-1, -k ,dtype = float32)
-    b = full(N, 2*k-Ss/dt,dtype = float32)
-    c = full(N-1, -k ,dtype = float32)
+    a = full(N - 1, -k, dtype=float32)
+    a[0] = -2 * (1 - alpha) * K * 4 / (3 * (dz) ** 2)
+    a[-2] = -(1 - alpha) * K * 4 / (3 * (dz) ** 2)
+    b = full(N, 2 * k - Ss / dt, dtype=float32)
+    b[1] = (1 - alpha) * K * 4 / (dz) ** 2 - Ss / dt
+    b[-2] = (1 - alpha) * K * 4 / (dz) ** 2 - Ss / dt
+    c = full(N - 1, -k, dtype=float32)
+    c[1] = -(1 - alpha) * K * 4 / (3 * (dz) ** 2)
+    c[-1] = -8 * (1 - alpha) * K / (3 * (dz) ** 2)
 
     lim = tri_product(a, b, c, H_prev)
 
     lim[0], lim[-1] = H0, Hn
 
-    k = K*alpha/dz**2
+    k = K * alpha / dz ** 2
 
-    a = full(N-1, k ,dtype = float32)
+    a = full(N - 1, k, dtype=float32)
+    a[0] = 8 * alpha * K / (3 * ((dz) ** 2))
+    a[-2] = (alpha) * K * 4 / (3 * (dz) ** 2)
     a[-1] = 0
-    b = full(N, -2*k-Ss/dt, dtype = float32)
+    b = full(N, -2 * k - Ss / dt, dtype=float32)
     b[0], b[-1] = 1, 1
-    c = full(N-1, k ,dtype = float32)
+    b[1] = -alpha * K * 4 / ((dz) ** 2) - Ss / dt
+    b[-2] = -(alpha) * K * 4 / ((dz) ** 2) - Ss / dt
+    c = full(N - 1, k, dtype=float32)
     c[0] = 0
+    c[1] = 4 * alpha * K / (3 * ((dz) ** 2))
+    c[-1] = 8 * (alpha) * K / (3 * (dz) ** 2)
 
     return solver(a, b, c, lim)
